5e26a26d4c313dc7d9a581305fcd1136c7558eb6
shenhongbang <shenhongbang@163.com>
2016-05-24 14:50:31 +0800

    添加 Carthage 支持

---
+++ AVPlayerDemo/SHBPlayer.h
@@ -0,0 +1,15 @@
+//
+//  SHBPlayer.h
+//  AVPlayerDemo
+//
+//  Created by shenhongbang on 16/5/24.
+//  Copyright © 2016年 shenhongbang. All rights reserved.
+//
+
+#ifndef SHBPlayer_h
+#define SHBPlayer_h
+
+#import "SHBPlayerController.h"
+
+
+#endif /* SHBPlayer_h */
